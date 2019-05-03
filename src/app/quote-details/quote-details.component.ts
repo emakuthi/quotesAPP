38c8b26f1f9054581a65bdf2091e59d94dd3ddc6
emakuthi <emakuthi@gmail.com>
2019-05-03 14:03:58 +0300

    add bootstrap styling

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -9,7 +9,7 @@ import {Quote} from '../quote'
 export class QuoteDetailsComponent implements OnInit {
 @Input()quote:Quote;
 @Output() isComplete= new EventEmitter<boolean>();
-quoteComplete(complete:boolean){
+quoteDelete(complete:boolean){
   this.isComplete.emit(complete);
 }
   constructor() { }
