06b249021032c7128ef07448379435e75b3afb7d
emakuthi <emakuthi@gmail.com>
2019-05-03 12:32:33 +0300

    creates directive

---
+++ src/app/highligh-quote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlighQuoteDirective } from './highligh-quote.directive';
+
+describe('HighlighQuoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlighQuoteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
